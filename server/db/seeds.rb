8ab31342a0e21e58f161fcd5d0505f80676af41a
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-31 17:34:28 -0400

    Add `rushing.json` seeds to Player Table
    Add the data in `rushing.json` to the Player table so that Rails can
    read, search and sort the data for the application.

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+data = JSON.parse(File.read("#{File.dirname(__FILE__)}/../../rushing.json"))
+
+data.each do |attributes|
+  Player.create!(
+    player: attributes['Player'],
+    team: attributes['Team'],
+    pos: attributes['Pos'],
+    att: attributes['Att'],
+    attg: attributes['Att/G'],
+    yds: attributes['Yds'],
+    avg: attributes['Avg'],
+    ydsg: attributes['Yds/G'],
+    td: attributes['TD'],
+    lng: attributes['Lng'],
+    sortByLng: attributes['Lng'],
+    first: attributes['1st'],
+    first_precentage: attributes['1st%'],
+    twenty_plus: attributes['20+'],
+    fourty_plus: attributes['40+'],
+    fum: attributes['FUM']
+  )
+end
