7612f4d54abe99e989e51272a7aae7cb1f925797
Jason Stathopulos <jstathopulos@gmail.com>
2020-05-31 16:34:18 -0400

    Update App to Get Players Data From Rails
    Update the App component to get the player's data from Rails to display
    the data coming to form the server to the user.

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,71 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+import { waitFor, screen, render } from '@testing-library/react'
+
+import axios from 'axios'
+
+import { Player } from './types/Players'
+
+import App from './App'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+const players: Player[] = [
+  {
+    attributes: {
+      att: 3,
+      attg: '0.8',
+      avg: '-0.7',
+      first: 0,
+      first_precentage: '0.0',
+      fourty_plus: 0,
+      fum: 1,
+      lng: 0,
+      player: 'Brett Hundley',
+      pos: 'QB',
+      td: 0,
+      team: 'GB',
+      twenty_plus: 0,
+      yds: -2,
+      ydsg: '-0.5',
+    },
+  },
+]
+
+describe('App', () => {
+  describe('fetch player data', () => {
+    it('renders loading screen', async () => {
+      render(<App />)
+
+      expect(screen.getByText('Loading', { exact: false })).toBeInTheDocument()
+      expect(screen.getByRole('main')).toMatchSnapshot()
+
+      await waitFor(() => {})
+    })
+
+    it('renders players component', async () => {
+      mockedAxios.get.mockResolvedValueOnce({ data: { data: players } })
+
+      render(<App />)
+
+      await waitFor(() => screen.getByRole('heading'))
+
+      expect(screen.getByRole('heading')).toHaveTextContent('Player')
+      expect(screen.getByRole('main')).toMatchSnapshot()
+    })
+
+    describe('on error from the server', () => {
+      it('should render an error message', async () => {
+        mockedAxios.get.mockRejectedValue(new Error('fake error message'))
+
+        render(<App />)
+
+        await waitFor(() => screen.getByText('error', { exact: false }))
+
+        expect(screen.getByText('error', { exact: false })).toBeInTheDocument()
+        expect(screen.getByRole('main')).toMatchSnapshot()
+      })
+    })
+  })
+})
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
+
+import { Player } from './types/Players'
+
+import PlayersTable from './components/PlayersTable'
+
+import './App.css'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [players, setPlayers] = useState<Player[]>([])
+  const url = `${process.env.REACT_APP_BASE_API_URL}/players`
+
+  useEffect(() => {
+    const fetchDataFromApi = async () => {
+      try {
+        const res = await axios.get(url)
+        // console.log('res', res.data.data)
+        setPlayers(res.data.data)
+        setIsLoading(false)
+      } catch (error) {
+        setErrorMessage(error.message)
+        setIsLoading(false)
+      }
+    }
+
+    fetchDataFromApi()
+  }, [url, setIsLoading, setErrorMessage])
+
+  if (isLoading) {
+    return (
+      <main className="App">
+        <div className="loading">Loading...</div>
+      </main>
+    )
+  }
+
+  const Body = () => {
+    if (errorMessage) {
+      return <section className="error">{errorMessage}</section>
+    }
+
+    return (
+      <section className="players">
+        <PlayersTable players={players} />
+      </section>
+    )
+  }
+
   return (
-    <div className="App">
+    <main className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Players</h1>
       </header>
-    </div>
-  );
+
+      <Body />
+    </main>
+  )
 }
 
-export default App;
+export default App
