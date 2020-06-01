aef1bbd0bc9cc8c452e3f989c81f2a69f413442f
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-01 17:44:21 -0400

    Add Sorting to Players Model
    Add the sorting to the Players model so that the frontend can choose how
    it should display the data to the user. Also, the reason that the
    sorting is happening at the array level instead of the database is that
    Rails still does not check for SQL injection when running queries.
    
    Since the frontend control what sorted, a bad actor could send a SQL
    statement into the order statement and attack in that vector. Having the
    order by at the array level lowerest the change that a SQL injection
    attack could happen.

--- server/app/models/player.rb
+++ server/app/models/player.rb
@@ -1,7 +1,19 @@
 class Player < ApplicationRecord
-  def self.search(name = nil)
-    return Player.all if name.nil?
+  def self.search(name = nil, sort_by = nil, sort_by_dir = nil)
+    players =
+      name.nil? ?
+        Player.all
+      :
+        Player.where("LOWER(player) LIKE ?", "%#{name.downcase}%")
 
-    Player.where("LOWER(player) LIKE ?", "%#{name.downcase}%")
+    if sort_by.nil? || players.has_attribute?(sort_by) == false
+      players
+    else
+      if sort_by_dir.nil? == false && sort_by_dir == 'desc'
+        players.sort_by(&sort_by.to_sym).reverse
+      else
+        players.sort_by(&sort_by.to_sym)
+      end
+    end
   end
 end
--- server/spec/models/player_spec.rb
+++ server/spec/models/player_spec.rb
@@ -25,5 +25,46 @@ RSpec.describe Player, type: :model do
         expect(found_all_joes.count).to eq(0)
       end
     end
+
+    context 'when sorting' do
+      before(:each) do
+        create_list(:player, 10)
+
+        @test_player = create(
+          :player,
+          player: "Jackie #{Faker::Name.last_name}",
+          yds: 11,
+          td: -20
+        )
+      end
+
+      it 'sort by Yds on all players' do
+        all_found = Player.search(nil, 'yds', 'desc')
+
+        expect(all_found.first['player']).to eq(@test_player['player'])
+      end
+
+      it 'sort by TD on players name Jackie' do
+        all_found = Player.search(nil, 'td', 'asc')
+
+        expect(all_found.first['player']).to eq(@test_player['player'])
+      end
+
+      context 'when no sort by direction is set' do
+        it 'sort by ascending order by default' do
+          all_found = Player.search(nil, 'td')
+
+          expect(all_found.first['player']).to eq(@test_player['player'])
+        end
+      end
+
+      context 'when sort by field does not exist' do
+        it 'unsorted results' do
+          all_found = Player.search(nil, 'not_a_feild')
+
+          expect(all_found.last['player']).to eq(@test_player['player'])
+        end
+      end
+    end
   end
 end
