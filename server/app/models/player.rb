f2e878dba87c70a3967e50dc3dfc095097d00c09
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-01 14:45:19 -0400

    Add Search Method to Player Model
    Add the search method to the Player model can search for all players if
    there is no name stated or players that are close to the name the user
    has started.
    
    ---
    
    Ref:
    https://stackoverflow.com/questions/28649702/rails-query-with-ilike

--- server/app/models/player.rb
+++ server/app/models/player.rb
@@ -1,2 +1,7 @@
 class Player < ApplicationRecord
+  def self.search(name = nil)
+    return Player.all if name.nil?
+
+    Player.where("LOWER(player) LIKE ?", "%#{name.downcase}%")
+  end
 end
--- server/spec/models/player_spec.rb
+++ server/spec/models/player_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Player, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#search' do
+    it 'find all players' do
+      create_list(:player, 10)
+
+      all_found = Player.search
+
+      expect(all_found.count).to eq(10)
+    end
+
+    it 'find all players name Zoe' do
+      create_list(:player, 2, player: "Zoe #{Faker::Name.last_name}")
+
+      found_all_joes = Player.search('zoe')
+
+      expect(found_all_joes.count).to eq(2)
+    end
+
+    context 'when no players are found' do
+      it 'should be an empty array' do
+        found_all_joes = Player.search('bob')
+
+        expect(found_all_joes.count).to eq(0)
+      end
+    end
+  end
 end
