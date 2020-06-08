e2a5b1d1b0ca8edacd9d315eb7b2f8186eaa563d
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-08 15:02:29 -0400

    Add Code Coverage Ignore to Create Context
    Add code coverage ignore to create context as there is no way for
    developers nor the user to access the dispatch directly. As a result, no
    test, require to test that path so that this line can safely ignore.
    
    With this update, code coverage on the frontend is now at 100%.

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -30,11 +30,15 @@ const initialState: InitialState = {
   isLoading: true,
 }
 
+/* istanbul ignore next */
 const AppContext = createContext<{
   state: InitialState
   dispatch: Dispatch<any>
 }>({
   state: initialState,
+  // Add code coverage ignore to create context as there is no way for
+  // developers nor the user to access the dispatch directly. As a result, no
+  // test, require to test that path so that this line can safely ignore.
   dispatch: () => null,
 })
 
