e7be7daf8395956d98e7999ffabbd3f0c4d05702
Jason Stathopulos <jstathopulos@gmail.com>
2020-06-08 14:18:27 -0400

    Add Page Select Test to Players Table Component
    Add the Page Select test to the Players Table component to show that the
    change page event is working.

--- frontend/src/components/PlayersTable.test.tsx
+++ frontend/src/components/PlayersTable.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 
 import { MakeWrapper, players } from './helpers/jest_helpers'
 
 import PlayersTable from './PlayersTable'
+import { Types, Action } from '../types/Actions'
 
 describe('PlayersTable', () => {
   it('should render as expeected', () => {
@@ -51,6 +52,30 @@ describe('PlayersTable', () => {
       )
 
       expect(queryByTestId('pagination')).not.toBeInTheDocument()
+    })
+
+    it('should switch to another page on click on click', () => {
+      const { getByRole } = render(
+        <MakeWrapper
+          state={{
+            players,
+            pagination: {
+              rows_per_page: 10,
+              page_total: 2,
+              current_page: '1',
+            },
+          }}
+          dispatch={(action: Action) => {
+            if (action.type === Types.UpdateURL) {
+              expect(action.payload.params.page).toEqual('2')
+            }
+          }}
+        >
+          <PlayersTable />
+        </MakeWrapper>
+      )
+
+      fireEvent.click(getByRole('button', { name: 'Go to page 2' }))
     })
   })
 })
